b8b467660fc8d168759cf63ded6d4b9b180190bb
marko-zc <marko.zarkovic.cerneli@gmail.com>
2022-07-11 18:33:50 +0200

    Inital commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sortOptions = [
+  {
+    label: "Chart",
+    value: "chart"
+  },
+  {
+    label: "Longest",
+    value: "longest"
+  },
+  {
+    label: "Shortest",
+    value: "shortest"
+  }
+]
+
+
+class App extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+        error: null,
+        isLoaded: false,
+        data: null,
+        selectedSortOption: "chart"
+    }
+      this.changeSort = this.changeSort.bind(this);
+  }
+  async componentDidMount()
+    {
+      await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
+      .then(response => response.json())
+      .then(
+        (data) => {
+          this.setState({
+              isLoaded: true,
+              tracks: data.tracks.data,
+              error: data.error
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+
+
+    }
+
+    changeSort = (e) => {
+      this.setState({ selectedSortOption: e.target.value });
+      if (e.target.value === "chart"){
+        this.setState({ tracks: this.state.tracks.sort((a, b) => {return a.position - b.position})})
+      } else if (e.target.value === "longest") {
+        this.setState({ tracks: this.state.tracks.sort((a, b) => {return b.duration - a.duration})})
+      } else if (e.target.value === "shortest") {
+        this.setState({ tracks: this.state.tracks.sort((a, b) => {return a.duration - b.duration})})
+      }
+    }
+
+  render()
+  {
+    const { error, isLoaded, tracks } = this.state;
+    if(error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded){
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div>
+          <h1>Top tracks:</h1>
+          <label htmlFor="sort">Sort by: </label>
+          <select defaultValue={this.state.selectedSortOption} onChange={this.changeSort}>
+            {sortOptions.map((sortOption) => (
+              <option value={sortOption.value}>{sortOption.label}</option>
+            ))}
+          </select>
+
+          <ul>
+            {tracks.map((track) => <li>{track.title}</li>)}
+          </ul>
+        </div>
+      )
+    }
+  }
 }
 
 export default App;
