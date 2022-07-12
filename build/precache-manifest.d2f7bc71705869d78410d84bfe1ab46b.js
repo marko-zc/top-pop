39c756926fa378409df1afd122eb805c1f2fd038
marko-zc <marko.zarkovic.cerneli@gmail.com>
2022-07-12 21:17:47 +0200

    Add build.

---
+++ build/precache-manifest.d2f7bc71705869d78410d84bfe1ab46b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "962efb694f7c084292e1",
+    "url": "/static/js/main.962efb69.chunk.js"
+  },
+  {
+    "revision": "d5b7fca4124f9150bc0d",
+    "url": "/static/js/1.d5b7fca4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10c247218e9f16272d7e6d0c1b939ee4",
+    "url": "/index.html"
+  }
+];
