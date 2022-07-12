dbddd704a0db24dd489eda3149a53501618d4df9
marko-zc <marko.zarkovic.cerneli@gmail.com>
2022-07-12 21:21:44 +0200

    Prepare for publishing on gh-pages

---
+++ build/precache-manifest.8a1cf172c456dd2bfa347203761a55de.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "962efb694f7c084292e1",
+    "url": "/top-pop/static/js/main.962efb69.chunk.js"
+  },
+  {
+    "revision": "d5b7fca4124f9150bc0d",
+    "url": "/top-pop/static/js/1.d5b7fca4.chunk.js"
+  },
+  {
+    "revision": "47520d3ca9389992cbf7",
+    "url": "/top-pop/static/js/runtime~main.47520d3c.js"
+  },
+  {
+    "revision": "6e8258aa50a5a40a66a2c2e0a1dd04b1",
+    "url": "/top-pop/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2f7bc71705869d78410d84bfe1ab46b.js"
+  "/top-pop/precache-manifest.8a1cf172c456dd2bfa347203761a55de.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/top-pop/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
